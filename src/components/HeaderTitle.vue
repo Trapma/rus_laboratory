166499e508f9c57e23a6f6bcd455cfe800aada27
Mikhail Trapeznikov <trapma@yandex.ru>
2022-08-01 04:43:18 +1000

    add HeaderTitle component

---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 class="h1">{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: "header-title",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.h1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 28px;
+
+  color: white;
+}
+</style>
