96403ade4329695f472adbc732e515b5e9631489
Mikhail Trapeznikov <trapma@yandex.ru>
2022-08-01 04:53:43 +1000

    create HomePage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/views/HomePage.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class="wrapper">
+    <h3 class="title">Описание</h3>
+    <p class="info">
+      Сделать приложение с 3 страницами, визуализирующие данные с
+      <a href="https://jsonplaceholder.typicode.com/">jsonplaceholder</a> .
+      Можно выбрать 3 разных любых эндпоинта апи во вкладке resources. При
+      выполнении задания необходимо использовать vuex/pinia и vue-router.
+      Стилизовать приложение не обязательно.
+    </p>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 24px;
+}
+.info {
+  margin-top: 30px;
+  padding: 0 30px;
+  font-size: 20px;
+  font-family: "Courier New", Courier, monospace;
+}
+</style>
